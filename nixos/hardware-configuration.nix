ed0f8a937e1d9e395f5786a3212759b285d0635a
David Gruber <85552617+codingmiracle@users.noreply.github.com>
2024-04-12 10:01:28 +0200

    Update hardware-configuration.nix

--- nixos/hardware-configuration.nix
+++ nixos/hardware-configuration.nix
@@ -1,7 +1,33 @@
-# This is just an example, you should generate yours with nixos-generate-config and put it in here.
+{ config, lib, pkgs, modulesPath, ... }:
+
 {
-  fileSystems."/" = {
-    device = "/dev/sda1";
-    fsType = "ext4";
-  };
+  imports =
+    [ (modulesPath + "/installer/scan/not-detected.nix")
+    ];
+
+  boot.initrd.availableKernelModules = [ "xhci_pci" "thunderb>
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ "kvm-intel" ];
+  boot.extraModulePackages = [ ];
+
+  fileSystems."/" =
+    { device = "/dev/disk/by-uuid/c09d6df7-c9a5-4032-a491-477>
+      fsType = "ext4";
+    };
+
+  fileSystems."/boot" =
+    { device = "/dev/disk/by-uuid/75F3-5EC4";
+      fsType = "vfat";
+    };
+
+  swapDevices = [ ];
+
+  # Enables DHCP on each ethernet and wireless interface. In >  # (the default) this is the recommended approach. When usin>
+  # still possible to use this option, but it's recommended t>
+  # with explicit per-interface declarations with `networking>
+  networking.useDHCP = lib.mkDefault true;
+  # networking.interfaces.enp0s31f6.useDHCP = lib.mkDefault t>
+
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.h>
 }
